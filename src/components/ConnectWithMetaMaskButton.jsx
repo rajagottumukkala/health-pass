afc33331141ef279615a2983ddde84b12314bf4a
rajagottumukkala <rajagottumukkala3@gmail.com>
2022-01-26 15:33:26 -0800

    added material-ui

--- src/components/ConnectWithMetaMaskButton.jsx
+++ src/components/ConnectWithMetaMaskButton.jsx
@@ -1,42 +1,55 @@
 import React from "react";
+import Button from '@mui/material/Button';
+
 
 import { connectWallet } from "../utils/common.js";
+import { Avatar } from "@mui/material";
 
 export default function ConnectWithMetaMaskButton({ setCurrentAccount }) {
   return (
-    <div style={{ maxWidth: "1280px", margin: "auto" }}>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          margin: "auto",
-          width: "max-content",
-          flexDirection: "column",
-          marginTop: "40px",
-        }}
+    <Button
+      variant="contained"
+      onClick={() => connectWallet(setCurrentAccount)}
+      startIcon={<Avatar src={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/2048px-MetaMask_Fox.svg.png'} />}
       >
-        <button
-          className=""
-          onClick={() => connectWallet(setCurrentAccount)}
-          style={{
-            background: "black",
-            color: "white",
-            padding: "8px 16px",
-            borderRadius: "4px",
-          }}
-        >
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/2048px-MetaMask_Fox.svg.png"
-            style={{
-              width: "40px",
-              display: "inline",
-              marginRight: "8px",
-            }}
-            alt="MetaMask Fox"
-          />
-          Connect with MetaMask
-        </button>
-      </div>
-    </div>
+      Connect with MetaMask
+    </Button>
   );
+
+  // return (
+  //   <div style={{ maxWidth: "1280px", margin: "auto" }}>
+  //     <div
+  //       style={{
+  //         display: "flex",
+  //         alignItems: "center",
+  //         margin: "auto",
+  //         width: "max-content",
+  //         flexDirection: "column",
+  //         marginTop: "40px",
+  //       }}
+  //     >
+  //       <button
+  //         className=""
+  //         onClick={() => connectWallet(setCurrentAccount)}
+  //         style={{
+  //           background: "black",
+  //           color: "white",
+  //           padding: "8px 16px",
+  //           borderRadius: "4px",
+  //         }}
+  //       >
+  //         <img
+  //           src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/2048px-MetaMask_Fox.svg.png"
+  //           style={{
+  //             width: "40px",
+  //             display: "inline",
+  //             marginRight: "8px",
+  //           }}
+  //           alt="MetaMask Fox"
+  //         />
+  //         Connect with MetaMask
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
 }
